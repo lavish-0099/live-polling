b465fb55490694c3f9ca7b9011f94f035812a4e2
lavish-0099 <lavishbatra1804@gmail.com>
2026-06-23 13:39:03 +0530

    fix

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -2,18 +2,18 @@ import { io } from "socket.io-client";
 
 export const socket = io(
   import.meta.env.VITE_SOCKET_URL
-  
-);
-import { io } from "socket.io-client";
-
-export const socket = io(
-  import.meta.env.VITE_SOCKET_URL
 );
 
 socket.on("connect", () => {
-  console.log("SOCKET CONNECTED:", socket.id);
+  console.log(
+    "SOCKET CONNECTED:",
+    socket.id
+  );
 });
 
 socket.on("connect_error", (err) => {
-  console.log("SOCKET ERROR:", err);
+  console.log(
+    "SOCKET ERROR:",
+    err.message
+  );
 });
